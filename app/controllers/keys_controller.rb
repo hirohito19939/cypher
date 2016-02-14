c96c75732894f4363f97573c129354e3171dcbb8
hirohito19939 <hirohito19939@gmail.com>
2016-02-14 18:16:23 +0900

    コミット７

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -8,10 +8,13 @@ class KeysController < ApplicationController
   end
 
   def destroy
+  @key = Key.where(cyfer_id: params[:id], user_id: current_user.id)
+  binding.pry
+  @key.destroy
   end
 
   private
   def create_params
-  params.permit(:cyfer_id).merge(user_id: current_user.id)
+  params.permit(:cyfer_id).merge(user_id: current_user.id, cyfer_id: params[:format])
 end
 end
